66fb1e78aea371d2b29a92fc9a74678940708cff
salman165324 <salmansadi165324@gmail.com>
2023-10-26 18:34:56 +0600

    Added a basic navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" >
       <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
         <ClerkProvider
           appearance={{
