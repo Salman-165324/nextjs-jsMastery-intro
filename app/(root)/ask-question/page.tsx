a362abd664ed761aa778663dab75b17811be6ccf
salman165324 <salmansadi165324@gmail.com>
2023-10-26 13:44:15 +0600

    implemented theme provider

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 const page = () => {
+  console.log("Where am I? Server of client")
   return (
     <div>Ask Question</div>
   )
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,8 @@ import React from "react";
 // eslint-disable-next-line camelcase
 import { Inter, Space_Grotesk } from "next/font/google";
 import type { Metadata } from "next/types";
-import "./globals.css"; 
+import "./globals.css";
+import { ThemeProvider } from "@/context/ThemeProvider";
 
 export const metadata: Metadata = {
   title: "DevFlow",
@@ -32,21 +33,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider
-     appearance={{
-      elements: {
-        formButtonPrimary: 'primary-gradient', 
-        footerActionLink: 'primary-text-gradient hover:text-primary-500'
-      }
-     }}
-    >
-      <html lang="en">
-        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
-          {children}
-          <h1 className="">This is some text</h1>
-        </body>
-        
-      </html>
-    </ClerkProvider>
+    <html lang="en">
+      <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+        <ClerkProvider
+          appearance={{
+            elements: {
+              formButtonPrimary: "primary-gradient",
+              footerActionLink: "primary-text-gradient hover:text-primary-500",
+            },
+          }}
+        >
+          <ThemeProvider>{children}</ThemeProvider>
+        </ClerkProvider>
+        <h1 className="">This is some text</h1>
+      </body>
+    </html>
   );
 }
